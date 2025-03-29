82634e4314af5b5951d8a5d571cc2ec9eb259f67
Muhammad <danishkanirya9999@gmail.com>
2025-03-29 16:34:09 +0500

    implementing jwt token generation logic and make login end point

--- Transactify/Controllers/UserAuthController.cs
+++ Transactify/Controllers/UserAuthController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.CodeDom.Compiler;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Transactify.Data;
 using Transactify.Models;
 
@@ -18,27 +23,27 @@ namespace Transactify.Controllers
         private readonly int _jwtExpiry;
 
         public UserAuthController(UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signinManager,IConfiguration configuration)
+            SignInManager<ApplicationUser> signinManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signinManager = signinManager;
             _jwtKey = configuration["Jwt:Key"];
-            _jwtIssuer=configuration["Jwt:Issuer"];
-            _jwtAudience= configuration["Jwt:Audience"];
-            _jwtExpiry=int.Parse( configuration["Jwt:Expiry"]);
+            _jwtIssuer = configuration["Jwt:Issuer"];
+            _jwtAudience = configuration["Jwt:Audience"];
+            _jwtExpiry = int.Parse(configuration["Jwt:Expiry"]);
         }
 
         //baseUrl/api/UserAuth/Register
         [HttpPost("Register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            if(model == null||string.IsNullOrEmpty(model.Name)
-                || string.IsNullOrEmpty(model.Email) 
+            if (model == null || string.IsNullOrEmpty(model.Name)
+                || string.IsNullOrEmpty(model.Email)
                 || string.IsNullOrEmpty(model.Password))
             {
                 return BadRequest(new { result = "Error", message = "Invalid payload" });
             }
-           var existingUser = await _userManager.FindByEmailAsync(model.Email);
+            var existingUser = await _userManager.FindByEmailAsync(model.Email);
             if (existingUser != null)
             {
                 return Conflict("Email already exists");
@@ -54,9 +59,50 @@ namespace Transactify.Controllers
             {
                 return Ok(new { result = "Success", message = "User created successfully" });
             }
-            return BadRequest(new { result = "Error", message = "User creation failed" });
+            return BadRequest(result.Errors);
         }
 
 
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                return Unauthorized(new { success = false, message = "Invalid email or password" });
+            }
+
+            var result = await _signinManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized(new { success = false, message = "Invalid email or password" });
+            }
+            var token = GenerateJWTToken(user);
+            return Ok(new { success = true, token });
+        }
+
+
+
+        private string GenerateJWTToken(ApplicationUser user)
+        {
+            var Claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub ,user.Id),
+                new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim("Name",user.Name)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token=new JwtSecurityToken(
+               
+                claims: Claims,
+                expires: DateTime.Now.AddMinutes(_jwtExpiry),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
