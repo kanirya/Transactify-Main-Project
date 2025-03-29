8febf5a15a0a4c556a1e9ede6a78e6ea5bdddde2
Muhammad <danishkanirya9999@gmail.com>
2025-03-29 15:41:58 +0500

    Adding complete configuation and making registration endpoint

---
+++ Transactify/Controllers/UserAuthController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Transactify.Data;
+using Transactify.Models;
+
+namespace Transactify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly string? _jwtKey;
+        private readonly string? _jwtIssuer;
+        private readonly string? _jwtAudience;
+        private readonly int _jwtExpiry;
+
+        public UserAuthController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signinManager,IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _jwtKey = configuration["Jwt:Key"];
+            _jwtIssuer=configuration["Jwt:Issuer"];
+            _jwtAudience= configuration["Jwt:Audience"];
+            _jwtExpiry=int.Parse( configuration["Jwt:Expiry"]);
+        }
+
+        //baseUrl/api/UserAuth/Register
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if(model == null||string.IsNullOrEmpty(model.Name)
+                || string.IsNullOrEmpty(model.Email) 
+                || string.IsNullOrEmpty(model.Password))
+            {
+                return BadRequest(new { result = "Error", message = "Invalid payload" });
+            }
+           var existingUser = await _userManager.FindByEmailAsync(model.Email);
+            if (existingUser != null)
+            {
+                return Conflict("Email already exists");
+            }
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                Name = model.Name
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                return Ok(new { result = "Success", message = "User created successfully" });
+            }
+            return BadRequest(new { result = "Error", message = "User creation failed" });
+        }
+
+
+    }
+}
--- Transactify/Program.cs
+++ Transactify/Program.cs
@@ -1,11 +1,48 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Transactify.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+
+// JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false, // or false if you don't want to validate
+        ValidateAudience = false, // or false if you don't want to validate
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    });
+
 
 var app = builder.Build();
 
